3cf62857e5aefc3039f5cbb4ab5943a8aa918414
israellefraim <israell.efraim@gmail.com>
2025-04-10 14:36:03 -0300

    Estrutura de condição if else

---
+++ estrutura-condicao.js
@@ -0,0 +1,11 @@
+let idade = 16;
+
+if (idade < 13) {
+	console.log("É criança");
+} else if (idade < 18) {
+	console.log("É adolescente");
+} else if (idade < 60) {
+	console.log("É adulto");
+} else {
+	console.log("É idoso"); 
+}
